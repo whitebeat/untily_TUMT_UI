3bb7c312bff67b4fcc6c4997b59280dc882d3efc
unknown <why123787065@gmail.com>
2019-10-24 14:56:15 +0800

    腳本

---
+++ UI/Assets/scripts/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;  //引用 音頻 api
+
+public class GameManager : MonoBehaviour
+{
+   public AudioMixer mixer;
+
+   public void Setvbgm(float value)
+    {
+        mixer.SetFloat("vbgm", value);
+    }
+}
