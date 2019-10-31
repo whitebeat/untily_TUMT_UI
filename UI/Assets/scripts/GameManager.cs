d5ea07e5ca8ef3afaf87037c066d3c2d83781e30
whitebeat <55227107+whitebeat@users.noreply.github.com>
2019-10-31 14:47:45 +0800

    場經

--- UI/Assets/scripts/GameManager.cs
+++ UI/Assets/scripts/GameManager.cs
@@ -1,12 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;  //引用 音頻 api
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
    public AudioMixer mixer;
+   public Text textloading;
+   public Slider loading;
+
 
    public void Setvbgm(float value)
     {
         mixer.SetFloat("vbgm", value);
+    }
+   public void Setvsfx(float value)
+    {
+        mixer.SetFloat("vsfx", value);
+    }
+   public void play()
+    {
+        SceneManager.LoadScene("場經");
     }
 }
