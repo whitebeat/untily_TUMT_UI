589bea5b740bf0968c9e9f3ad98c65536ebd52d6
whitebeat <55227107+whitebeat@users.noreply.github.com>
2019-12-12 14:49:14 +0800

    陷阱完成

---
+++ UI/Assets/scripts/player.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+    public int hp = 100;
+    public Slider slider;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject);
+
+        if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<trap>().damage;
+            hp -= d;
+            slider.value = hp;
+
+        }
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<trap>().damage;
+            hp -= d;
+            slider.value = hp;
+
+        }
+    }
+}
---
+++ UI/Assets/scripts/trap.cs
@@ -0,0 +1,8 @@
+﻿
+using UnityEngine;
+
+public class trap : MonoBehaviour
+{
+    [Header("傷害值"), Range(1, 50)]
+    public int damage = 10;
+}
