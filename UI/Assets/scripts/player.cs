2227ce0b7fc6a4e75a089fe48fec045b9cc5d38b
whitebeat <55227107+whitebeat@users.noreply.github.com>
2019-12-19 15:31:25 +0800

    時間完成

--- UI/Assets/scripts/player.cs
+++ UI/Assets/scripts/player.cs
@@ -4,9 +4,30 @@ using UnityEngine.UI;
 
 public class player : MonoBehaviour
 {
+    [Header("血量與血條")]
     public int hp = 100;
     public Slider slider;
 
+    [Header("壽司區域")]
+    public Text textMaki;
+    public int MakiCount;
+    public int MakiTotal;
+    [Header("時間區域")]
+    public Text textTime;
+    public float gameTime;
+
+    public Text TextMaki
+    {
+        get
+        {
+            return textMaki;
+        }
+
+        set
+        {
+            textMaki = value;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -19,6 +40,16 @@ public class player : MonoBehaviour
             slider.value = hp;
 
         }
+        if (other.tag == "Maki")
+        {
+            MakiCount++;
+            textMaki.text = "Maki:" + MakiCount + "/" + MakiTotal;
+            Destroy(other.gameObject);
+        }
+        if (other.name =="總點" && MakiCount == MakiTotal)
+        {
+            print("過關");
+        }
     }
     private void OnParticleCollision(GameObject other)
     {
@@ -29,5 +60,23 @@ public class player : MonoBehaviour
             slider.value = hp;
 
         }
+       
+       
+    }
+
+    private void Start()
+    {
+        MakiTotal = GameObject.FindGameObjectsWithTag("Maki").Length;
+        textMaki.text = "Maki:0/" + MakiTotal;
+    }
+
+    public void Update()
+    {
+        UpdateTime();
+    }
+    private void UpdateTime()
+    {
+        gameTime += Time.deltaTime;
+        textTime.text = gameTime.ToString("F2");
     }
 }
